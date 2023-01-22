823a4087abf8654c1e5669cd52e1157fdd788d03
Adela Spirkova <ad.spirkova@gmail.com>
2023-01-22 18:41:37 +0100

    Final

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch("../components/header.html")
+     .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("header").innerHTML = data;
+    });
+
+    fetch("../components/navbar.html")
+     .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("nav").innerHTML = data;
+    });
+
+    fetch("../components/footer.html")
+     .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("footer").innerHTML = data;
+    });
+});
